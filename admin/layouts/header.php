43787f552bfef60609c807e6e717b439fac049b0
lviethai1997 <lviethai1997@gmail.com>
2019-05-03 19:23:48 +0700

    asd

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -25,12 +25,8 @@
 
         <!-- Custom CSS -->
         <link href="<?php echo base_url() ?>public/admin/dist/css/sb-admin-2.css" rel="stylesheet">
-        
-        <script src="//cdn.ckeditor.com/4.11.3/standard/ckeditor.js"></script>
-        
-        
+        <script src="//cdn.ckeditor.com/4.11.4/full/ckeditor.js"></script>
         <script src="<?php echo base_url()?>public/ckfinder/ckfinder.js"></script>
-
         <!-- Custom Fonts -->
         <link href="<?php echo base_url() ?>public/admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     </head>
@@ -367,7 +363,6 @@
                 <!-- /.navbar-static-side -->
             </nav>
             <!-- Page Content -->
-           
             <div id="page-wrapper" style="min-height: 435px;">
                 <div class="container-fluid">
                 
